d7cc1e1cfd4d375527c1b1fe45fd5e54c88df56c
John Vincent Belandres <belandresjohnvincent@gmail.com>
2021-07-01 15:28:00 +0800

    Added error message if email is not registered.

--- InterviewBot/user/views.py
+++ InterviewBot/user/views.py
@@ -47,6 +47,8 @@ def password_reset_request(request):
 					except BadHeaderError:
 						return HttpResponse('Invalid header found.')
 					return redirect ("password_reset_done")
+			else:
+				messages.error(request, "Email not registered.")
 	password_reset_form = PasswordResetForm()
 	return render(request=request, template_name="password/password_reset.html", context={"password_reset_form":password_reset_form})
 
