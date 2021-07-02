4947152121a478aa04df4905c5221b755f27ff8b
John Vincent Belandres <belandresjohnvincent@gmail.com>
2021-07-02 16:04:29 +0800

    Updated job offers css and added tile for filter fields in job offers (user-side)

--- InterviewBot/user/filters.py
+++ InterviewBot/user/filters.py
@@ -3,8 +3,8 @@ from django.forms.widgets import TextInput
 from .models import CreateJob
 
 class JobSearchFilter(django_filters.FilterSet):
-	title = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder':'Title'}))
-	description = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder':'Description'}))
+	title = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder':'Title', 'title':'Search job by title, keywords...'}))
+	description = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder':'Description', 'title':'Search job by description keywords...'}))
 
 	class Meta:
 		model = CreateJob
