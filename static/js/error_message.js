f1ef5fbf9bb78e63c43c9e69dfc1c3b673217ee9
John Vincent <belandresjohnvincent@gmail.com>
2021-10-18 12:02:12 +0800

    Added some jquery stuffs and moved the javascript to a js file

---
+++ static/js/error_message.js
@@ -0,0 +1,9 @@
+$(function (){
+    $('#register input[type="text"]').on("input", function(){
+        $('.error_message').hide();
+    });
+
+    $('#register input[type="text"]').on("input", function(){
+        $('.error_message').hide();
+    });
+});
