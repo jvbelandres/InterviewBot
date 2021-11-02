3627f67a71ca2bae23a8ea0e578ae5d7eea6865a
John Vincent <belandresjohnvincent@gmail.com>
2021-11-02 15:28:38 +0800

    Overriding create method in serializers for mobile

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,45 @@
 from rest_framework import serializers
+
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+
+from .tokens import account_activation_token
 from .models import Account, AppliedJob, Contact, CreateJob, SavedJob
-from user import models
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ('email', 'is_active', 'staff', 'admin', 'firstname', 'lastname', 'phone', 'password', 'gender')
+
+    def create(self, validated_data):
+        newUser =  Account(
+            email = validated_data['email'],
+            is_active = validated_data['is_active'],
+            staff = validated_data['staff'],
+            admin = validated_data['admin'],
+            firstname = validated_data['firstname'],
+            lastname = validated_data['lastname'],
+            phone = validated_data['phone'],
+            gender = validated_data['gender'],
+        )
+        newUser.set_password(validated_data['password'])
+        newUser.save()
+
+        mail_subject = 'Activate your account.'
+        message = render_to_string('account_activation/user/acc_active_email.html', {
+            'user': newUser,
+            'domain': "127.0.0.1:8000",
+            'uid':urlsafe_base64_encode(force_bytes(newUser.pk)),
+            'token':account_activation_token.make_token(newUser),
+        })
+        to_email = validated_data['email']
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
+        return newUser
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
