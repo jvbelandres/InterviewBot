fffb028d77cd2bd873dc5e90359042ffd0245040
Rodel Perez <69116852+rodel250@users.noreply.github.com>
2022-02-12 15:57:46 +0800

    Update views.py

--- InterviewBot/views.py
+++ InterviewBot/views.py
@@ -151,7 +151,7 @@ class JobOfferingsAdminListViewAPI(ListAPIView):
 
 	def get_queryset(self):
 		admin_id = self.kwargs['admin_id']
-		return CreateJob.objects.filter(is_deleted=False)
+		return CreateJob.objects.filter(admin_id=admin_id,is_deleted=False)
 
 class AppliedJobApplicantsListViewAPI(ListAPIView):
 	permission_classes = (IsAuthenticated,)
